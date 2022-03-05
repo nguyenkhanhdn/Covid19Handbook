2996906bb31a564c0601ac1de916ac6f8c6ca9c8
Khanh Nguyen <khanhdsp@gmail.com>
2022-03-05 15:34:29 +0700

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Covid19Handbook.Startup))]
+namespace Covid19Handbook
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
